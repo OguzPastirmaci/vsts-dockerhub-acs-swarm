f39fa16effba14df9752efb79ca48376841fd84f
Oguz Pastirmaci <oguzp@microsoft.com>
2015-12-17 15:53:16 -0800

    Fix scale variable

--- deploy-to-acs.sh
+++ deploy-to-acs.sh
@@ -1,4 +1,4 @@
 chmod 400 ssh-cert
 scp -P 2200 -i ssh-cert docker-compose.yml azureuser@oguzp-acs3mgmt.japaneast.cloudapp.azure.com:/home/azureuser
-ssh -i ssh-cert azureuser@oguzp-acs3mgmt.japaneast.cloudapp.azure.com -A -p 2200 'export DOCKER_HOST=10.0.0.5:2375; docker-compose up -d; docker-compose scale web=$3'
+ssh -i ssh-cert azureuser@oguzp-acs3mgmt.japaneast.cloudapp.azure.com -A -p 2200 'export DOCKER_HOST=10.0.0.5:2375; docker-compose stop; docker-compose rm; docker-compose up -d; docker-compose scale web=3'
 
