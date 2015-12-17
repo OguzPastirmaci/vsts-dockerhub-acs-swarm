e68f597c9d0722c9aff3e7ff71396b0f9ec28745
Oguz Pastirmaci <oguzp@microsoft.com>
2015-12-17 15:39:39 -0800

    Added key

--- deploy-to-acs.sh
+++ deploy-to-acs.sh
@@ -1,4 +1,4 @@
-chmod 400 $1
+chmod 400 ssh-cert
 scp -P 2200 -i $1 docker-compose.yml $2:/home/azureuser
 ssh -i $1 -o StrictHostKeyChecking=no $2 'export DOCKER_HOST=10.0.0.5:2375; docker-compose up -d; docker-compose scale web=$3'
 
